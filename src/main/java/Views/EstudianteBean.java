633e6decc6ca8d66384a395fdb20364a6d72fa52
d.ramirez <juandavidramirezmendoza@hotmail.com>
2017-11-02 22:38:59 -0500

    Cambio de usuario

--- src/main/java/Views/EstudianteBean.java
+++ src/main/java/Views/EstudianteBean.java
@@ -9,16 +9,6 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author 2110805
- */
 
 @ManagedBean(name="beanSolicitudEstudiante")
 @SessionScoped
