7366f90ada23ccf1487ca87cfd8ff14d1c50f6c7
j.cuesta <jonathancuesta97@gmail.com>
2017-11-15 17:40:39 -0500

    correcion daos

--- src/main/java/persistencia/EstudianteDAO.java
+++ src/main/java/persistencia/EstudianteDAO.java
@@ -7,6 +7,7 @@ package persistencia;
 
 import entidades.Estudiante;
 import entidades.SolicitudCancelacion;
+import java.util.List;
 
 /**
  *
@@ -14,7 +15,7 @@ import entidades.SolicitudCancelacion;
  */
 public interface EstudianteDAO {
     public Estudiante loadByID(int id);
-    public void loadAll();
+    public List<Estudiante> loadAll();
     //public void actualizarEstudiante(Estudiante p);
     public void save(int idEstudiante, SolicitudCancelacion solicitudCancelacion);
     
