28fa947f5e84e21e3f193f12c7c531f6f1baac20
j.cuesta <jonathancuesta97@gmail.com>
2017-11-16 20:15:37 -0500

    mapper corregido

--- src/test/java/Tests/ServiciosCancelacionesTest.java
+++ src/test/java/Tests/ServiciosCancelacionesTest.java
@@ -168,7 +168,7 @@ public class ServiciosCancelacionesTest{
     public void testUnaMateriaCanceladaAnteriormenteNoPuedeVolverHacerCancelada() throws ExcepcionServiciosCancelaciones{
         
 } 
-    /*
+    
     @Test
     public void prueba(){
         ServiciosCancelaciones a = ServiciosCancelacionesFactory.getInstance().getServiciosCancelaciones();
@@ -179,12 +179,15 @@ public class ServiciosCancelacionesTest{
             System.out.println(x.getCorreo());
             System.out.println(x.getAcudiente().getNombre());
             System.out.println(x.getConsejero().getNombre());
+            
+            System.out.println(x.getMateriasCursadas());
+            
 
         }catch(Exception e){
             e.printStackTrace();
             fail();
         }
-    }*/
+    }
    
 }
 
